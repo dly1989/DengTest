f005e9eea4d8892c823f1dc5210944f60679b9fd
chenrui2200 <dly1989@example.com>
2024-11-25 22:09:44 +0800

    123

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
 
     # 我想测试下提交更新aws， 今天天气下雨了
-    # 再测试下
+    # 再测试下423424243
     print('context', context)
     return {
         'statusCode': 200,
